f0b38a53c08a8a33a0e379fbc40f54e45971952c
qsaifudin <duafinis@gmail.com>
2023-10-05 11:39:23 +0700

    tes

--- src/models/index.js
+++ src/models/index.js
@@ -1,23 +1,26 @@
 const { Sequelize } = require("sequelize-cockroachdb");
 const { QueryTypes } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.PGHOST, {
-  operatorsAliases: 0,
-  logging: 0,
-  define: {
-    freezeTableName: 1,
-  },
-  pool: {
-    max: 5,
-    min: 0,
-    acquire: 30000,
-    idle: 10000,
-  },
-  dialectOptions: {
-    useUTC: false,
-  },
-  timezone: "+07:00",
-});
+const sequelize = new Sequelize(
+  "postgresql://saifudin:GTcOh5DbFqE_iHQ-wU-Xjg@minty-werebat-6591.8nk.cockroachlabs.cloud:26257/test_database?sslmode=verify-full",
+  {
+    operatorsAliases: 0,
+    logging: 0,
+    define: {
+      freezeTableName: 1,
+    },
+    pool: {
+      max: 5,
+      min: 0,
+      acquire: 30000,
+      idle: 10000,
+    },
+    dialectOptions: {
+      useUTC: false,
+    },
+    timezone: "+07:00",
+  }
+);
 
 const db = {};
 db.Sequelize = Sequelize;

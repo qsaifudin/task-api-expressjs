c739bcb66e2a2f8a71b8656c1f7eb124784d96ea
qsaifudin <duafinis@gmail.com>
2023-10-05 11:23:40 +0700

    route

--- src/routes/taskRoutes.js
+++ src/routes/taskRoutes.js
@@ -1,19 +1,19 @@
 const controller = require("../controllers/taskController");
 
 module.exports = (app) => {
-  const router = require("express").Router();
+  // const router = require("express").Router();
 
-  router.get("/", (req, res) => {
+  app.get("/", (req, res) => {
     res.send("Saifudin Task API ExpressJS");
   });
 
   // Group routes under /api/v1 prefix
   // Get a list of tasks
-  app.use("/API/v1", router);
+  // app.use("/API/v1", app);
 
-  router.get("/tasks", controller.getAll); // Get a list of tasks
-  router.get("/tasks/:id", controller.getById); // Get a specific task by ID
-  router.post("/tasks", controller.create); // Create a new task
-  router.put("/tasks/:id", controller.update); // Update a task by ID
-  router.delete("/tasks/:id", controller.delete); // Delete a task by ID
+  app.get("/API/v1/tasks", controller.getAll); // Get a list of tasks
+  app.get("/API/v1/tasks/:id", controller.getById); // Get a specific task by ID
+  app.post("/API/v1/tasks", controller.create); // Create a new task
+  app.put("/API/v1/tasks/:id", controller.update); // Update a task by ID
+  app.delete("/API/v1/tasks/:id", controller.delete); // Delete a task by ID
 };

4636f104cf6f95b6d942cc5b2c61296accb8315b
qsaifudin <duafinis@gmail.com>
2023-10-05 11:34:40 +0700

    tes

---
+++ src/app1.js
@@ -0,0 +1,38 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleware
+app.use(cors());
+app.use(bodyParser.json());
+
+// Connect to database
+const db = require("./models");
+
+// Synchronize the database with the defined models (create tables if they don't exist)
+db.sequelize.sync();
+
+// Routes
+require("./routes/taskRoutes")(app);
+
+// Error handling middleware for unmatched routes
+app.use((req, res, next) => {
+  res.status(404).json({ status: false, message: "Route not found" });
+});
+
+// Error handling middleware for general errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ status: false, message: "Something went wrong!" });
+});
+
+// Start the server
+app.listen(port, () => {
+  console.log(`Server listening on port http://localhost:${port}`);
+});
+
+module.exports = app;

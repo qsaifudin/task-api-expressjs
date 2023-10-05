3e69b90be251cc62ffd67c44f985021b2220c241
qsaifudin <duafinis@gmail.com>
2023-10-05 08:00:13 +0700

    connect cockroachdb

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-npx sequelize-cli model:generate --name Task --attributes title:string,description:string,completed:boolean
-npx sequelize-cli db:migrate
+npm run dev
+npm test
--- models/index.js
+++ models/index.js
@@ -1,28 +1,23 @@
-const { Sequelize, QueryTypes } = require("sequelize");
-const sequelize = new Sequelize(
-  process.env.PGDATABASE,
-  process.env.PGUSER,
-  process.env.PGPASSWORD,
-  {
-    host: process.env.PGHOST,
-    dialect: process.env.PGDIALECT,
-    operatorsAliases: 0,
-    logging: 0,
-    define: {
-      freezeTableName: 1,
-    },
-    pool: {
-      max: 5,
-      min: 0,
-      acquire: 30000,
-      idle: 10000,
-    },
-    dialectOptions: {
-      useUTC: false,
-    },
-    timezone: "+07:00",
-  }
-);
+const { Sequelize } = require("sequelize-cockroachdb");
+const { QueryTypes } = require("sequelize");
+
+const sequelize = new Sequelize(process.env.PGHOST, {
+  operatorsAliases: 0,
+  logging: 0,
+  define: {
+    freezeTableName: 1,
+  },
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000,
+  },
+  dialectOptions: {
+    useUTC: false,
+  },
+  timezone: "+07:00",
+});
 
 const db = {};
 db.Sequelize = Sequelize;
